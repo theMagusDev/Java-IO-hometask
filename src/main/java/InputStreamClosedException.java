444a53ed141ecacfbf431d11e65b57eac73d5501
Yuriy Magus <114507764+theMagusDev@users.noreply.github.com>
2024-11-03 12:49:44 +0300

    Fix bugs and handle different error scenarios

---
+++ src/main/java/InputStreamClosedException.java
@@ -0,0 +1,8 @@
+public class InputStreamClosedException extends Exception {
+    public InputStreamClosedException() {
+        super();
+    }
+    public InputStreamClosedException(String message) {
+        super(message);
+    }
+}
